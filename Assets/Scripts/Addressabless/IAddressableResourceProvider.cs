de06b8165efc3c58e2bd3de3489c7e0941131d29
igor_nebosenko <igor.nebosenko@gmail.com>
2022-11-06 22:33:57 +0200

    - Addressabless groups - Resource provider interface

---
+++ Assets/Scripts/Addressabless/IAddressableResourceProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace IM.Addressabless
+{
+    public interface IAddressableResourceProvider
+    {
+        Task<T> GetAddressableAsync<T>(string path) where T : Object;
+    }
+}
